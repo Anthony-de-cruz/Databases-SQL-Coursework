54dceaba77b502862f5aebcffde8b6dd19f43aae
Anthony de Cruz <anthonydaviddecruz@gmail.com>
2023-04-26 14:54:49 +0100

    Updated tables

--- sql/data-definition/CREATE TABLE FlightBooking.sql
+++ sql/data-definition/CREATE TABLE FlightBooking.sql
@@ -4,17 +4,19 @@ CREATE TABLE FlightBooking (
     FlightID INTEGER NOT NULL,
     NumSeats INTEGER NOT NULL
         CHECK (0 < NumSeats),
-    Status CHAR(1) NOT NULL,
+    Status CHAR(1) NOT NULL
+        CHECK (Status = 'R' OR Status = 'C')
+        DEFAULT 'R',
     BookingTime TIMESTAMP NOT NULL
         CHECK (BookingTime >= CURRENT_TIMESTAMP)
         DEFAULT CURRENT_TIMESTAMP,
     TotalCost DECIMAL
         CHECK (0 < TotalCost),
     PRIMARY KEY (BookingID),
-    FOREIGN KEY (CustomerID) REFERENCES leadcustomer(customerid)
+    FOREIGN KEY (CustomerID) REFERENCES leadcustomer(CustomerID)
         ON DELETE RESTRICT
         ON UPDATE RESTRICT,
-    FOREIGN KEY (FlightID) REFERENCES flight(flightid)
+    FOREIGN KEY (FlightID) REFERENCES flight(FlightID)
         ON DELETE RESTRICT
         ON UPDATE RESTRICT
 );
